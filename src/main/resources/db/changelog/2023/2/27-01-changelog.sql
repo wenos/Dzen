ad699ce6e9e7221da798f0a9d391debc508204d4
Ramal <ramalguseynov03@gmail.com>
2024-03-24 13:09:20 +0300

    Поправил создание категорий

---
+++ src/main/resources/db/changelog/2023/2/27-01-changelog.sql
@@ -0,0 +1,5 @@
+INSERT INTO c_category (id, title)
+VALUES
+    (nextval('category_id_seq'), 'Другое'),
+    (nextval('category_id_seq'), 'Category 2'),
+    (nextval('category_id_seq'), 'Category 3')
